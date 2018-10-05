c95b5445937f87a790306ab9e7023864362a45ba
mtondolo <mtondolo@gmail.com>
2018-10-05 13:37:46 +0200

    Show loading progress

--- app/src/main/java/com/android/example/comesanews/LatestNewsActivity.java
+++ app/src/main/java/com/android/example/comesanews/LatestNewsActivity.java
@@ -18,6 +18,8 @@ package com.android.example.comesanews;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.example.comesanews.utils.LatestNewsJSONUtils;
@@ -28,6 +30,8 @@ import java.net.URL;
 public class LatestNewsActivity extends AppCompatActivity {
 
     private TextView mLatestNewsListTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +41,51 @@ public class LatestNewsActivity extends AppCompatActivity {
         // Using findViewById, we get a reference to our TextView from xml.
         mLatestNewsListTextView = (TextView) findViewById(R.id.tv_latest_news);
 
+        /* This TextView is used to display errors and will be hidden if there are no errors */
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+        /*
+         * The ProgressBar that will indicate to the user that we are loading data. It will be
+         * hidden when no data is loading.
+         *
+         */
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
         /* Once all of our views are setup, we can load the latest news data. */
         loadLatestNewsData();
     }
 
     // This method will tell some background method to get the latest news data in the background.
     private void loadLatestNewsData() {
+        showLatestNewsDataView();
         new FetchLatestNewsTask().execute();
     }
 
+    /**
+     * This method will make the View for the latest news data visible and
+     * hide the error message.
+     */
+    private void showLatestNewsDataView() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mLatestNewsListTextView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the news
+     * View.
+     */
+    private void showErrorMessage() {
+        mLatestNewsListTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
     public class FetchLatestNewsTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
         @Override
         protected String doInBackground(String... params) {
             URL latestNewsRequestUrl = NetworkUtils.buildLatestNewsUrl();
@@ -64,10 +103,14 @@ public class LatestNewsActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String latestNewsData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (latestNewsData != null) {
-                // Append the Strings to the TextView.
+                showLatestNewsDataView();
                 mLatestNewsListTextView.append((latestNewsData) + "\n\n\n");
+            } else {
+                showErrorMessage();
             }
         }
     }
 }
+

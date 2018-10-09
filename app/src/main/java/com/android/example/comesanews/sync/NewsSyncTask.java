6cf423f873c9cd07321cc625c892569bd45cf942
mtondolo <mtondolo@gmail.com>
2018-10-09 12:46:44 +0200

    Schedule data syncing

--- app/src/main/java/com/android/example/comesanews/sync/NewsSyncTask.java
+++ app/src/main/java/com/android/example/comesanews/sync/NewsSyncTask.java
@@ -20,44 +20,25 @@ public class NewsSyncTask {
 
         try {
 
-            /*
-             * The getUrl method will return the URL that we need to get the policy JSON for the
-             * policy.
-             *
-             */
             URL newsRequestUrl = NetworkUtils.buildLatestNewsUrl();
-
-            /* Use the URL to retrieve the JSON */
             String jsonNewsResponse = NetworkUtils.getResponseFromHttpUrl(newsRequestUrl);
 
-            /* Parse the JSON into a list of policy values */
             ContentValues[] newsValues = LatestNewsJSONUtils.getSimpleNewsStringsFromJson(context, jsonNewsResponse);
 
-            /*
-             * In cases where our JSON contained an error code, getSimpleYCPolicyStringFromJson
-             * would have returned null.
-             */
-
             if (newsValues != null && newsValues.length != 0) {
-                /* Get a handle on the ContentResolver to delete and insert data */
                 ContentResolver newsContentResolver = context.getContentResolver();
-
-                /* Delete old policy data because we don't need to keep multiple days' data */
                 newsContentResolver.delete(
                         NewsContract.LatestNewsEntry.CONTENT_URI,
                         null,
                         null);
 
-                /* Insert our new policy data into Policy's ContentProvider */
                 newsContentResolver.bulkInsert(
                         NewsContract.LatestNewsEntry.CONTENT_URI,
                         newsValues);
             }
 
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }

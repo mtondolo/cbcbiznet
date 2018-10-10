dc02d5ae355fc84e0e259e619408a443665f2e10
mtondolo <mtondolo@gmail.com>
2018-10-10 08:34:12 +0200

    Revert "Schedule data syncing"
    This reverts commit 6cf423f873c9cd07321cc625c892569bd45cf942.

--- app/src/main/java/com/android/example/comesanews/sync/NewsSyncUtils.java
+++ app/src/main/java/com/android/example/comesanews/sync/NewsSyncUtils.java
@@ -4,69 +4,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import com.android.example.comesanews.data.NewsContract;
-import com.firebase.jobdispatcher.Constraint;
-import com.firebase.jobdispatcher.Driver;
-import com.firebase.jobdispatcher.FirebaseJobDispatcher;
-import com.firebase.jobdispatcher.GooglePlayDriver;
-import com.firebase.jobdispatcher.Job;
-import com.firebase.jobdispatcher.Lifetime;
-import com.firebase.jobdispatcher.Trigger;
-
-import java.util.concurrent.TimeUnit;
 
 public class NewsSyncUtils {
 
-    // Interval at which to sync with the news. This is just for test purposes.
-    private static final int REMINDER_INTERVAL_MINUTES = 1;
-    private static final int REMINDER_INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES));
-    private static final int SYNC_FLEXTIME_SECONDS = REMINDER_INTERVAL_SECONDS;
-
     private static boolean sInitialized;
-
-    //  Sync tag to identify our sync job
-    private static final String NEWS_SYNC_TAG = "news-sync";
-
-    // Schedules a repeating sync of news data using FirebaseJobDispatcher.
-    static void scheduleFirebaseJobDispatcherSync(@NonNull final Context context) {
-
-        Driver driver = new GooglePlayDriver(context);
-        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
-
-        /* Create the Job to periodically sync News */
-        Job syncNewsJob = dispatcher.newJobBuilder()
-                .setService(NewsFirebaseJobService.class)
-                .setTag(NEWS_SYNC_TAG)
-                .setConstraints(Constraint.ON_ANY_NETWORK)
-                .setLifetime(Lifetime.FOREVER)
-                .setRecurring(true)
-                .setTrigger(Trigger.executionWindow(
-                        REMINDER_INTERVAL_SECONDS,
-                        REMINDER_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
-                .setReplaceCurrent(true)
-                .build();
-
-        // Schedule the Job with the dispatcher
-        dispatcher.schedule(syncNewsJob);
-    }
 
     // Creates periodic sync tasks and checks to see if an immediate sync is required.
     synchronized public static void initialize(@NonNull final Context context) {
 
+        // Only perform initialization once per app lifetime.
         if (sInitialized) return;
+
+        // If the method body is executed, set sInitialized to true
         sInitialized = true;
 
-        // This method call triggers News to create its task to synchronize news data periodically.
-        scheduleFirebaseJobDispatcherSync(context);
+        new AsyncTask<Void, Void, Void>() {
 
-        // We need to check to see if our ContentProvider has data to display in our news list.
-        Thread checkForEmpty = new Thread(new Runnable() {
             @Override
-            public void run() {
+            protected Void doInBackground(Void... voids) {
+                /* URI for every row of policy data in our policy table*/
                 Uri newsQueryUri = NewsContract.LatestNewsEntry.CONTENT_URI;
+
+                /*
+                 * Since this query is going to be used only as a check to see if we have any
+                 * data (rather than to display data), we just need to PROJECT the ID of each
+                 * row.
+                 */
                 String[] projectionColumns = {NewsContract.LatestNewsEntry._ID};
+
+                /* Here, we perform the query to check to see if we have any news data */
                 Cursor cursor = context.getContentResolver().query(
                         newsQueryUri,
                         projectionColumns,
@@ -74,16 +44,20 @@ public class NewsSyncUtils {
                         null,
                         null);
 
+                /*
+                 * If the Cursor was null OR if it was empty, we need to sync immediately to
+                 * be able to display data to the user.
+                 */
                 if (null == cursor || cursor.getCount() == 0) {
                     startImmediateSync(context);
                 }
 
+                /* Close the Cursor to avoid memory leaks! */
                 cursor.close();
+                return null;
             }
-        });
+        }.execute();
 
-        // Finally, once the thread is prepared, fire it off to perform our checks.
-        checkForEmpty.start();
     }
 
     public static void startImmediateSync(@NonNull final Context context) {

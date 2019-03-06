9876f0c51e88df72706641bf1dfebb8a8de6e2e9
mtondolo <mtondolo@gmail.com>
2019-03-06 12:29:27 +0200

    Add preferencefragment

---
+++ app/src/main/java/com/android/example/comesapp/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.android.example.comesapp;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+}

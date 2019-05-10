c33a4451751d9e2001c784d5793906890e34e41c
mtondolo <mtondolo@gmail.com>
2019-05-10 13:39:59 +0200

    Create first layout in eventsactivity

---
+++ app/src/main/java/com/android/example/comesapp/Events.java
@@ -0,0 +1,15 @@
+package com.android.example.comesapp;
+
+public class Events {
+
+    // This method returns a list of events.
+    public static String[] getEvents() {
+        return new String[]{
+                "The 2nd COMESA – Development Partners’ Coordination Forum",
+                "Money Laundering Sensitisation Workshop",
+                "Creation Awareness on Trade Based Money Laundering (TBML) Workshop",
+                "Training of Trainers (TOTs) Workshops",
+                "Focus Groups at Border Posts Workshops ",
+        };
+    }
+}
--- app/src/main/java/com/android/example/comesapp/EventsActivity.java
+++ app/src/main/java/com/android/example/comesapp/EventsActivity.java
@@ -2,12 +2,26 @@ package com.android.example.comesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EventsActivity extends AppCompatActivity {
+
+    private TextView mEventsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        // Using findViewById, we get a reference to our TextView from xml.
+        mEventsTextView = findViewById(R.id.tv_events);
+
+        // This String array contains events.
+        String[] events = Events.getEvents();
+
+        // Iterate through the array and append the Strings to the TextView
+        for (String event : events) {
+            mEventsTextView.append(event + "\n\n\n");
+        }
     }
 }

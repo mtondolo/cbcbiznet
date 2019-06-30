9d28afe7fcc2a9ae1b0530f5a4b7fcc6f09ad235
mtondolo <mtondolo@gmail.com>
2019-06-30 19:27:10 +0200

    Check options in navigation drawer

--- app/src/main/java/com/android/example/comesapp/NewsActivity.java
+++ app/src/main/java/com/android/example/comesapp/NewsActivity.java
@@ -96,33 +96,51 @@ public class NewsActivity extends AppCompatActivity implements
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mActionBarDrawerToggle = new ActionBarDrawerToggle
                 (this, mDrawerLayout, topToolbar, R.string.Open, R.string.Close);
-
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
 
         mNavigationView = findViewById(R.id.nv);
-        mNavigationView.setItemIconTintList(null);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch (id) {
-                    case R.id.events:
 
-                        // Intent to start events activity
-                        Intent eventsIntent = new Intent(NewsActivity.this, EventsActivity.class);
-                        startActivity(eventsIntent);
-                        break;
-                    case R.id.opportunities:
-                        Toast.makeText(NewsActivity.this, R.string.opportunities, Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
+        // Set events option as checked
+        Menu menu = mNavigationView.getMenu();
+        menu.findItem(R.id.nav_events).setChecked(true);
+
+        mNavigationView.setNavigationItemSelectedListener
+                (new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.nav_events:
+                                // Intent to start events activity
+                                Intent eventsIntent = new Intent
+                                        (NewsActivity.this, EventsActivity.class);
+                                startActivity(eventsIntent);
+
+                                // Set events option as checked
+                                // Set vacancies option as unchecked
+                                Menu menu = mNavigationView.getMenu();
+                                menu.findItem(R.id.nav_vacancies).setChecked(false);
+                                menu.findItem(R.id.nav_events).setChecked(true);
+
+                                break;
+                            case R.id.nav_vacancies:
+                                Toast.makeText
+                                        (NewsActivity.this, R.string.opportunities,
+                                                Toast.LENGTH_SHORT).show();
+
+                                // Set vacancies option as checked
+                                // Set events option as unchecked
+                                Menu menu_vacancies = mNavigationView.getMenu();
+                                menu_vacancies.findItem(R.id.nav_events).setChecked(false);
+                                menu_vacancies.findItem(R.id.nav_vacancies).setChecked(true);
+
+                                break;
+                            default:
+                                return true;
+                        }
                         return true;
-                }
-
-                return true;
-
-            }
-        });
+                    }
+                });
 
         // Lookup the swipe container view and set its properties
         mySwipeRefreshLayout = findViewById(R.id.swiperefresh);
